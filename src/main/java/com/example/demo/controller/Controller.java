10806c8daaea65bf66e516047ea2680b024f5da7
macoredroid <coredroid0401@gmail.com>
2020-12-18 10:46:37 +0800

    update

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+  @Autowired
+  WebApplicationContext applicationContext;
+
+
+  @RequestMapping(value = "/cpu", method = {RequestMethod.OPTIONS, RequestMethod.GET}, produces = "application/json")
+  public ResponseEntity<?> test() throws InterruptedException {
+    TestService testService=applicationContext.getBean(TestService.class);
+    return testService.RunCPUTest();
+  }
+
+
+}
---
+++ src/main/java/com/example/demo/serviceimpl/TestServiceImpl.java
@@ -0,0 +1,96 @@
+package com.example.demo.serviceimpl;
+
+import com.example.demo.service.TestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.LongAdder;
+
+@Service
+public class TestServiceImpl implements TestService {
+    @Override
+    public ResponseEntity<?> RunCPUTest() throws InterruptedException {
+        int numThreads = 16;
+
+
+
+        LongAdder counter = new LongAdder();
+
+        List<CalculationThread> runningCalcs = new ArrayList<>();
+        List<Thread> runningThreads = new ArrayList<>();
+
+        System.out.printf("Starting %d threads\n", numThreads);
+
+        for (int i = 0; i < numThreads; i++)
+        {
+            CalculationThread r = new CalculationThread(counter);
+            Thread t = new Thread(r);
+            runningCalcs.add(r);
+            runningThreads.add(t);
+            t.start();
+        }
+        double count=0;
+        for (int i = 0; i < 15; i++)
+        {
+            counter.reset();
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e)
+            {
+                break;
+            }
+            count+=(double)(counter.longValue()) / numThreads;
+            System.out.printf("[%d] Calculations per second: %d (%.2f per thread)\n",
+                    i,
+                    counter.longValue(),
+                    (double)(counter.longValue()) / numThreads
+            );
+        }
+        count=count/15;
+        for (int i = 0; i < runningCalcs.size(); i++)
+        {
+            runningCalcs.get(i).stop();
+            runningThreads.get(i).join();
+        }
+        return new ResponseEntity<>(String.valueOf(count), HttpStatus.OK);
+    }
+
+    public static class CalculationThread implements Runnable
+    {
+        private final Random rng;
+        private final LongAdder calculationsPerformed;
+        private boolean stopped;
+        private double store;
+
+        public CalculationThread(LongAdder calculationsPerformed)
+        {
+            this.calculationsPerformed = calculationsPerformed;
+            this.stopped = false;
+            this.rng = new Random();
+            this.store = 1;
+        }
+
+        public void stop()
+        {
+            this.stopped = true;
+        }
+
+        @Override
+        public void run()
+        {
+            while (! this.stopped)
+            {
+                double r = this.rng.nextFloat();
+                double v = Math.sin(Math.cos(Math.sin(Math.cos(r))));
+                this.store *= v;
+                this.calculationsPerformed.add(1);
+            }
+        }
+    }
+}
